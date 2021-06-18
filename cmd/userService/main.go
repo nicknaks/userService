99f598ac7496c107fb2e2bf6cc4cf0931adf80bb
nick_nak <nikita.nackaznoy@gmail.com>
2021-06-19 02:08:44 +0300

    Добавлен логгер + БД

--- cmd/userService/main.go
+++ cmd/userService/main.go
@@ -1,12 +1,44 @@
 package main
 
 import (
+	"github.com/jameycribbs/hare"
+	"github.com/jameycribbs/hare/datastores/disk"
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	log2 "github.com/labstack/gommon/log"
+	"log"
 	"userService/internal/pkg/user/delivery"
+	"userService/internal/pkg/user/repository"
+	"userService/internal/pkg/user/usecase"
 )
 
+func initDB() *hare.Database {
+	ds, err := disk.New("./data", ".json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	db, err := hare.New(ds)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	err = db.CreateTable("users")
+	return db
+}
+
 func setConfig(e *echo.Echo) {
-	userDelivery := delivery.UserDelivery{}
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Format: "method=${method}, uri=${uri}, status=${status}\n",
+	}))
+
+	e.Logger.SetLevel(log2.Lvl(0))
+
+	db := initDB()
+
+	userRepository := repository.UserRepository{DB: *db}
+	userUsecase := usecase.UserUsecase{Repository: &userRepository}
+	userDelivery := delivery.UserDelivery{Usecase: &userUsecase}
 	userDelivery.SetRoutersForUser(e)
 }
 
---
+++ go.mod
@@ -0,0 +1,12 @@
+module userService
+
+go 1.16
+
+require (
+	github.com/google/uuid v1.2.0 // indirect
+	github.com/jameycribbs/hare v0.6.0 // indirect
+	github.com/jcelliott/lumber v0.0.0-20160324203708-dd349441af25 // indirect
+	github.com/labstack/echo/v4 v4.3.0
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/nanobox-io/golang-scribble v0.0.0-20190309225732-aa3e7c118975 // indirect
+)
